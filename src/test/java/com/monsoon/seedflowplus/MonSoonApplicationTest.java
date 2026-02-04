ed518ec962e882379bf13ea4459c3696f440e32b
guntinue <rjssn93@gmail.com>
2026-02-04 09:01:44 +0900

    Jenkins CI Test

--- src/test/java/com/monsoon/seedflowplus/MonSoonApplicationTest.java
+++ src/test/java/com/monsoon/seedflowplus/MonSoonApplicationTest.java
@@ -1,7 +1,14 @@
 package com.monsoon.seedflowplus;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class MonSoonApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
 
 }
