00ac3edaa249c7c00262e9736d75734d1f737f02
hatahng <hathang16@naver.com>
2026-02-23 17:54:57 +0900

    BAC-13 feat(account): ClientCrop 엔티티 생성

---
+++ src/main/java/com/monsoon/seedflowplus/domain/account/ClientCrop.java
@@ -0,0 +1,24 @@
+package com.monsoon.seedflowplus.domain.account;
+
+import com.monsoon.seedflowplus.core.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "client_crops")
+public class ClientCrop extends BaseEntity {
+
+    @Column(nullable = false)
+    private String cropName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id")
+    private Client clientId;
+}
