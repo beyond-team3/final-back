e50dca6553b18d56d7893e63dd8a388cb96b6953
fijitlwkr <hakyung1133@gmail.com>
2026-02-24 11:57:53 +0900

    feat(erd): cultivationTime 연관관계 매핑 및 빌더 추가

--- src/main/java/com/monsoon/seedflowplus/domain/product/entity/CultivationTime.java
+++ src/main/java/com/monsoon/seedflowplus/domain/product/entity/CultivationTime.java
@@ -2,13 +2,12 @@ package com.monsoon.seedflowplus.domain.product.entity;
 
 import com.monsoon.seedflowplus.core.common.entity.BaseModifyEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "tbl_cultivation_time")
 public class CultivationTime extends BaseModifyEntity {
@@ -17,8 +16,9 @@ public class CultivationTime extends BaseModifyEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cultivationTimeId;
 
-    @Column(nullable = false)
-    private Long productId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", referencedColumnName = "product_id", nullable = false, unique = true)
+    private Product product;
 
     private Integer sowingStart;
 
@@ -31,4 +31,16 @@ public class CultivationTime extends BaseModifyEntity {
     private Integer harvestingStart;
 
     private Integer harvestingEnd;
+
+    @Builder
+    public CultivationTime(Product product, Integer sowingStart, Integer sowingEnd, Integer plantingStart,
+                           Integer plantingEnd, Integer harvestingStart, Integer harvestingEnd) {
+        this.product = product;
+        this.sowingStart = sowingStart;
+        this.sowingEnd = sowingEnd;
+        this.plantingStart = plantingStart;
+        this.plantingEnd = plantingEnd;
+        this.harvestingStart = harvestingStart;
+        this.harvestingEnd = harvestingEnd;
+    }
 }
