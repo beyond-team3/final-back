6182a59037bde0d2eaae3d1c06029b414eb31f71
hatahng <hathang16@naver.com>
2026-02-26 17:20:10 +0900

    [BAC-39] chore(security): CORS 허용 오리진 프로토콜 누락 수정

--- src/main/java/com/monsoon/seedflowplus/infra/security/SecurityConfig.java
+++ src/main/java/com/monsoon/seedflowplus/infra/security/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration
-                .setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:8000", "localhost:30090"));
+                .setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:8000", "http://localhost:30090"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setExposedHeaders(List.of("Authorization"));
