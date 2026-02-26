0abfb742d2c754656781114e1da01894c2375183
hatahng <hathang16@naver.com>
2026-02-26 22:08:28 +0900

    [BAC-39] feat(security): 사용자 상세 정보를 담는 CustomUserDetails 구현

---
+++ src/main/java/com/monsoon/seedflowplus/infra/security/CustomUserDetails.java
@@ -0,0 +1,66 @@
+package com.monsoon.seedflowplus.infra.security;
+
+import com.monsoon.seedflowplus.domain.account.entity.Role;
+import com.monsoon.seedflowplus.domain.account.entity.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final Long userId;
+    private final String loginId;
+    private final String password;
+    private final Role role;
+    private final boolean enabled;
+    private final User user; // 필요한 경우 엔티티 직접 참조
+
+    public CustomUserDetails(User user) {
+        this.userId = user.getId();
+        this.loginId = user.getLoginId();
+        this.password = user.getLoginPw();
+        this.role = user.getRole();
+        this.enabled = user.getStatus() == com.monsoon.seedflowplus.domain.account.entity.Status.ACTIVATE;
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return loginId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
