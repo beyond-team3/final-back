6edae45bf374a56b064bce66ca8866c5ab7f1613
LKM1027 <lkm031027@gmail.com>
2026-02-26 10:14:23 +0900

    [refactor] RequestMapping 주소 수정

--- src/main/java/com/monsoon/seedflowplus/domain/note/controller/NoteController.java
+++ src/main/java/com/monsoon/seedflowplus/domain/note/controller/NoteController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/notes")
+@RequestMapping("/api/v1/notes")
 @RequiredArgsConstructor
 public class NoteController {
 
