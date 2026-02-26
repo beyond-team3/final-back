f6d6fb6e08a3a5fafb22f470818c749c4ac46b71
hatahng <hathang16@naver.com>
2026-02-26 17:02:32 +0900

    [BAC-39] feat(account): 직원 정보 수정 요청 DTO에 이메일 형식 검증 추가

--- src/main/java/com/monsoon/seedflowplus/domain/account/dto/request/EmployeeUpdateRequest.java
+++ src/main/java/com/monsoon/seedflowplus/domain/account/dto/request/EmployeeUpdateRequest.java
@@ -1,8 +1,10 @@
 package com.monsoon.seedflowplus.domain.account.dto.request;
 
+import jakarta.validation.constraints.Email;
+
 public record EmployeeUpdateRequest(
         String employeeName,
-        String employeeEmail,
+        @Email(message = "이메일 형식이 올바르지 않습니다.") String employeeEmail,
         String employeePhone,
         String address) {
 }
