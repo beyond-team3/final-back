b497834173f117acdb9d8ca835f7dc70cbdcd2e5
hatahng <hathang16@naver.com>
2026-02-26 17:19:06 +0900

    [BAC-39] feat(account): 직원 정보 수정 요청 시 전화번호 형식 검증 추가

--- src/main/java/com/monsoon/seedflowplus/domain/account/dto/request/EmployeeUpdateRequest.java
+++ src/main/java/com/monsoon/seedflowplus/domain/account/dto/request/EmployeeUpdateRequest.java
@@ -1,10 +1,11 @@
 package com.monsoon.seedflowplus.domain.account.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 
 public record EmployeeUpdateRequest(
         String employeeName,
         @Email(message = "이메일 형식이 올바르지 않습니다.") String employeeEmail,
-        String employeePhone,
+        @Pattern(regexp = "^$|^[0-9-]{9,20}$", message = "전화번호 형식이 올바르지 않습니다.") String employeePhone,
         String address) {
 }
