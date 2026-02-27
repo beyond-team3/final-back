6d6081008ef69f0104ba961ebd41851dabc496df
hatahng <hathang16@naver.com>
2026-02-27 12:20:47 +0900

    [BAC-48] feat(account): 계정 미생성 거래처 조회를 위한 JPQL 쿼리 추가

--- src/main/java/com/monsoon/seedflowplus/domain/account/repository/ClientRepository.java
+++ src/main/java/com/monsoon/seedflowplus/domain/account/repository/ClientRepository.java
@@ -2,6 +2,7 @@ package com.monsoon.seedflowplus.domain.account.repository;
 
 import com.monsoon.seedflowplus.domain.account.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,5 +18,8 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
     boolean existsByClientBrnAndIdNot(String clientBrn, Long id);
 
     List<Client> findAllByManagerEmployeeId(Long employeeId);
+
+    @Query("SELECT c FROM Client c WHERE NOT EXISTS (SELECT 1 FROM User u WHERE u.client = c)")
+    List<Client> findAllUnregistered();
 
 }
