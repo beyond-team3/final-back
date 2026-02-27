0be00714f45e28577ca507be8739bb1727174487
fijitlwkr <hakyung1133@gmail.com>
2026-02-27 10:17:35 +0900

    fix(jenkins): jenkins 실행 프로필 확인 로그 추가

--- src/test/java/com/monsoon/seedflowplus/MonSoonApplicationTest.java
+++ src/test/java/com/monsoon/seedflowplus/MonSoonApplicationTest.java
@@ -1,14 +1,25 @@
 package com.monsoon.seedflowplus;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MonSoonApplicationTest {
 
+    @Autowired
+    private Environment env;
+
     @Test
     void contextLoads() {
 
+    }
+
+    @Test
+    void printProfile() {
+        System.out.println("### Active Profiles: " + Arrays.toString(env.getActiveProfiles()));
     }
 
 }
