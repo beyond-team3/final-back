0ea65936d4b77206690e56ec78ff5906df6b2c25
LKM1027 <lkm031027@gmail.com>
2026-02-27 12:30:17 +0900

    [feat] RequestMapping 주소 수정

--- src/main/java/com/monsoon/seedflowplus/domain/map/controller/PestMapController.java
+++ src/main/java/com/monsoon/seedflowplus/domain/map/controller/PestMapController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/map")
+@RequestMapping("/api/v1/map")
 @RequiredArgsConstructor
 public class PestMapController {
 
