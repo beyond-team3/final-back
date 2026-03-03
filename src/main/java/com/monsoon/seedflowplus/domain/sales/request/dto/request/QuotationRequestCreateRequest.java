0a0d9523a6b81feb8bb8a06dd95d64ff1b54d2e1
hatahng <hathang16@naver.com>
2026-03-03 22:12:53 +0900

    [BAC-58] feat(quotationrequest): 생성 DTO 내 중첩 유효성 검증 및 수량 제약 조건 추가

--- src/main/java/com/monsoon/seedflowplus/domain/sales/request/dto/request/QuotationRequestCreateRequest.java
+++ src/main/java/com/monsoon/seedflowplus/domain/sales/request/dto/request/QuotationRequestCreateRequest.java
@@ -1,17 +1,19 @@
 package com.monsoon.seedflowplus.domain.sales.request.dto.request;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 
 public record QuotationRequestCreateRequest(
         String requirements,
-        @NotEmpty(message = "품목은 최소 1개 이상이어야 합니다.") List<ItemRequest> items) {
+        @NotEmpty(message = "품목은 최소 1개 이상이어야 합니다.") List<@NotNull @Valid ItemRequest> items) {
     public record ItemRequest(
             Long productId,
             @NotBlank(message = "상품 품종은 필수입니다.") String productCategory,
             @NotBlank(message = "상품 이름은 필수입니다.") String productName,
-            @NotNull(message = "수량은 필수입니다.") Integer quantity) {
+            @NotNull(message = "수량은 필수입니다.") @Positive(message = "수량은 1 이상이어야 합니다.") Integer quantity) {
     }
 }
