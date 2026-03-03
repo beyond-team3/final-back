73a1712f110d00c59241680a6e60c16d9b1f11f9
guntinue <rjssn93@gmail.com>
2026-03-03 15:07:22 +0900

    BAC-44 refactor(product): 테스트 파일 비교상품 삭제범위 검증 수정

--- src/test/java/com/monsoon/seedflowplus/domain/product/repository/ProductCompareItemRepositoryTest.java
+++ src/test/java/com/monsoon/seedflowplus/domain/product/repository/ProductCompareItemRepositoryTest.java
@@ -34,36 +34,51 @@ class ProductCompareItemRepositoryTest {
     @DisplayName("product id로 관련된 모든 비교 아이템을 삭제할 수 있다.")
     void deleteAllByProductIdTest() {
         // given
-        Product product = Product.builder()
+        Product targetProduct = Product.builder()
                 .productCode("TEST-PD-01")
-                .productName("테스트 상품")
+                .productName("삭제 대상 상품")
                 .productCategory(ProductCategory.WATERMELON)
                 .amount(100)
                 .unit("BOX")
                 .price(new BigDecimal("10000"))
                 .status(ProductStatus.SALE)
                 .build();
-        Product savedProduct = productRepository.save(product);
+        Product savedTargetProduct = productRepository.save(targetProduct);
+
+        Product otherProduct = Product.builder()
+                .productCode("TEST-PD-02")
+                .productName("유지 대상 상품")
+                .productCategory(ProductCategory.WATERMELON)
+                .amount(50)
+                .unit("BOX")
+                .price(new BigDecimal("20000"))
+                .status(ProductStatus.SALE)
+                .build();
+        Product savedOtherProduct = productRepository.save(otherProduct);
 
         ProductCompare productCompare = ProductCompare.builder()
                 .title("테스트 비교 내역")
                 .build();
         ProductCompare savedCompare = productCompareRepository.save(productCompare);
 
-        ProductCompareItem item1 = ProductCompareItem.builder().product(savedProduct).build();
+        ProductCompareItem item1 = ProductCompareItem.builder().product(savedTargetProduct).build();
         item1.setProductCompare(savedCompare);
-        ProductCompareItem item2 = ProductCompareItem.builder().product(savedProduct).build();
+        ProductCompareItem item2 = ProductCompareItem.builder().product(savedTargetProduct).build();
         item2.setProductCompare(savedCompare);
 
-        productCompareItemRepository.saveAll(List.of(item1, item2));
+        ProductCompareItem otherItem = ProductCompareItem.builder().product(savedOtherProduct).build();
+        otherItem.setProductCompare(savedCompare);
+
+        productCompareItemRepository.saveAll(List.of(item1, item2, otherItem));
         productCompareItemRepository.flush();
 
         // when
-        productCompareItemRepository.deleteAllByProductId(savedProduct.getId());
+        productCompareItemRepository.deleteAllByProductId(savedTargetProduct.getId());
         productCompareItemRepository.flush();
 
         // then
         List<ProductCompareItem> remainingItems = productCompareItemRepository.findAll();
-        assertThat(remainingItems).isEmpty();
+        assertThat(remainingItems).hasSize(1);
+        assertThat(remainingItems.get(0).getProduct().getId()).isEqualTo(savedOtherProduct.getId());
     }
 }
