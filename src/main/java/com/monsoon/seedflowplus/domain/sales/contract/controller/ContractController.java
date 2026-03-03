caf8fea40a69498b570b53418a013a809fcd2e5e
hatahng <hathang16@naver.com>
2026-03-03 11:00:57 +0900

    [BAC-46] feat(contract): 신규 계약서 생성을 위한 저장 API(POST) 구현

--- src/main/java/com/monsoon/seedflowplus/domain/sales/contract/controller/ContractController.java
+++ src/main/java/com/monsoon/seedflowplus/domain/sales/contract/controller/ContractController.java
@@ -1,15 +1,14 @@
 package com.monsoon.seedflowplus.domain.sales.contract.controller;
 
 import com.monsoon.seedflowplus.core.common.support.response.ApiResult;
+import com.monsoon.seedflowplus.domain.sales.contract.dto.request.ContractCreateRequest;
 import com.monsoon.seedflowplus.domain.sales.contract.dto.response.ContractPrefillResponse;
 import com.monsoon.seedflowplus.domain.sales.contract.service.ContractService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Contract", description = "계약 API")
 @RestController
@@ -21,8 +20,14 @@ public class ContractController {
 
     @Operation(summary = "계약서 작성을 위한 견적 데이터 불러오기", description = "견적서 ID를 통해 계약서 작성에 필요한 기본 정보를 조회합니다.")
     @GetMapping("/prefill")
-    public ApiResult<ContractPrefillResponse> getPrefillData(@RequestParam Long quotationId) {
+    public ApiResult<ContractPrefillResponse> getPrefillData(@RequestParam("quotationId") Long quotationId) {
         return ApiResult.success(contractService.getPrefillData(quotationId));
     }
 
+    @Operation(summary = "계약서 저장", description = "새로운 계약서를 저장합니다.")
+    @PostMapping
+    public ApiResult<?> createContract(@RequestBody @Valid ContractCreateRequest request) {
+        contractService.createContract(request);
+        return ApiResult.success();
+    }
 }
