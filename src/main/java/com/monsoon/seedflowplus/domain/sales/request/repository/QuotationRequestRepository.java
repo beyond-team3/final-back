ab31454bfb1e93e30eac653155d2ea83bef9a925
hatahng <hathang16@naver.com>
2026-03-03 21:49:10 +0900

    [BAC-58] feat(quotationrequest): QuotationRequestRepository 내 상태별 및 거래처별 조회 메서드 추가

--- src/main/java/com/monsoon/seedflowplus/domain/sales/request/repository/QuotationRequestRepository.java
+++ src/main/java/com/monsoon/seedflowplus/domain/sales/request/repository/QuotationRequestRepository.java
@@ -1,9 +1,19 @@
 package com.monsoon.seedflowplus.domain.sales.request.repository;
 
+import com.monsoon.seedflowplus.domain.account.entity.Client;
 import com.monsoon.seedflowplus.domain.sales.request.entity.QuotationRequestHeader;
+import com.monsoon.seedflowplus.domain.sales.request.entity.QuotationRequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuotationRequestRepository extends JpaRepository<QuotationRequestHeader, Long> {
+    List<QuotationRequestHeader> findByStatus(QuotationRequestStatus status);
+
+    List<QuotationRequestHeader> findByStatusAndClient(QuotationRequestStatus status, Client client);
+
+    List<QuotationRequestHeader> findByStatusAndClientManagerEmployeeId(QuotationRequestStatus status,
+                                                                        Long managerEmployeeId);
 }
