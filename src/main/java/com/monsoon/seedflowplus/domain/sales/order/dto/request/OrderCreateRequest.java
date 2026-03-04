b857a25032346a67a3c6b052e5722273103bf445
fijitlwkr <hakyung1133@gmail.com>
2026-03-04 20:36:10 +0900

    BAC-72 refactor(order): 계약 헤더 식별자 필드명 headerId로 통일

--- src/main/java/com/monsoon/seedflowplus/domain/sales/order/dto/request/OrderCreateRequest.java
+++ src/main/java/com/monsoon/seedflowplus/domain/sales/order/dto/request/OrderCreateRequest.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Getter
 public class OrderCreateRequest {
 
-    private Long contractId;
+    private Long headerId; // reason: 계약 헤더 PK임을 detailId와 명확히 구분하기 위해 contractId를 headerId로 명확화
 
     // 배송 정보
     private String shippingName;
