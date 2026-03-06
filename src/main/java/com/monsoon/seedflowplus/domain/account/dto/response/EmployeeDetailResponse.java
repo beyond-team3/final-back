47b73cc75c5fde7ffa290ff9075785f2d4773634
hatahng <hathang16@naver.com>
2026-03-06 15:03:34 +0900

    [BAC-82] refactor(account): EmployeeDetailResponse 내 주소 파싱 적용 및 필드 확장

--- src/main/java/com/monsoon/seedflowplus/domain/account/dto/response/EmployeeDetailResponse.java
+++ src/main/java/com/monsoon/seedflowplus/domain/account/dto/response/EmployeeDetailResponse.java
@@ -1,25 +1,39 @@
 package com.monsoon.seedflowplus.domain.account.dto.response;
 
+import com.monsoon.seedflowplus.core.common.util.AddressParser;
+import com.monsoon.seedflowplus.domain.account.entity.Role;
 import com.monsoon.seedflowplus.domain.account.entity.Status;
 import com.monsoon.seedflowplus.domain.account.entity.User;
 
+import java.time.LocalDateTime;
+
 public record EmployeeDetailResponse(
-        String employeeName,
+        Long employeeId,
         String employeeCode,
+        String employeeName,
+        Role role,
         String employeeEmail,
         String employeePhone,
-        String address,
+        LocalDateTime createdAt,
+        String addressDetail,
+        String addressZip,
         Status status) {
     public static EmployeeDetailResponse from(User user) {
         if (user.getEmployee() == null) {
             return null;
         }
+        AddressParser.AddressInfo addressInfo = AddressParser.parse(user.getEmployee().getAddress());
+
         return new EmployeeDetailResponse(
-                user.getEmployee().getEmployeeName(),
+                user.getEmployee().getId(),
                 user.getEmployee().getEmployeeCode(),
+                user.getEmployee().getEmployeeName(),
+                user.getRole(),
                 user.getEmployee().getEmployeeEmail(),
                 user.getEmployee().getEmployeePhone(),
-                user.getEmployee().getAddress(),
+                user.getEmployee().getCreatedAt(),
+                addressInfo.detail(),
+                addressInfo.zip(),
                 user.getStatus());
     }
 }
