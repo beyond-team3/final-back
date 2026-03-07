4240b96414bcdbe11a321ce0ce7c885323c2be15
hatahng <hathang16@naver.com>
2026-03-08 03:50:21 +0900

    [BAC-88] feat(quotation): 만료된 견적서 조회를 위한 레포지토리 메서드 추가

--- src/main/java/com/monsoon/seedflowplus/domain/sales/quotation/repository/QuotationRepository.java
+++ src/main/java/com/monsoon/seedflowplus/domain/sales/quotation/repository/QuotationRepository.java
@@ -15,4 +15,7 @@ public interface QuotationRepository extends JpaRepository<QuotationHeader, Long
     List<QuotationHeader> findAllByStatus(QuotationStatus status);
 
     List<QuotationHeader> findAllByStatusAndAuthorId(QuotationStatus status, Long authorId);
+
+    // 상태와 만료일을 기준으로 견적서 조회
+    List<QuotationHeader> findByStatusAndExpiredDateLessThanEqual(QuotationStatus status, java.time.LocalDate date);
 }
