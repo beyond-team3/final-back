3099a390fb0880b0c01bcb7e69cf398dc0bab621
fijitlwkr <hakyung1133@gmail.com>
2026-03-10 04:59:01 +0900

    [BAC-94] fix(deal): 문서 조회 repository import 보정

--- src/main/java/com/monsoon/seedflowplus/domain/deal/core/repository/DocumentSummaryQueryRepositoryImpl.java
+++ src/main/java/com/monsoon/seedflowplus/domain/deal/core/repository/DocumentSummaryQueryRepositoryImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
