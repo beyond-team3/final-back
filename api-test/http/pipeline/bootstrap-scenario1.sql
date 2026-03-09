f3cac30aea1441eac445f6f1ccaf66e875b4d3d3
fijitlwkr <hakyung1133@gmail.com>
2026-03-10 03:59:39 +0900

    [BAC-93] chore(http-client): bootstrap SQL 초기화 방식 수정

--- api-test/http/pipeline/bootstrap-scenario1.sql
+++ api-test/http/pipeline/bootstrap-scenario1.sql
@@ -1,60 +1,98 @@
 -- SeedFlow+ scenario1 재실행용 초기화 + 계정/거래처 seed 스크립트
 -- 전제: MariaDB / MySQL
 -- 주의: 개발 DB 데이터를 삭제한 뒤 scenario1에 필요한 계정/거래처를 다시 만든다.
+-- 비어 있는 테이블에서도 멈추지 않도록 TRUNCATE 대신 DELETE + AUTO_INCREMENT 초기화를 사용한다.
+
+START TRANSACTION;
 
 SET FOREIGN_KEY_CHECKS = 0;
 
-TRUNCATE TABLE tbl_notification_delivery;
-TRUNCATE TABLE tbl_notification;
+DELETE FROM tbl_notification_delivery;
+DELETE FROM tbl_notification;
 
-TRUNCATE TABLE tbl_approval_decision;
-TRUNCATE TABLE tbl_approval_step;
-TRUNCATE TABLE tbl_approval_request;
+DELETE FROM tbl_approval_decision;
+DELETE FROM tbl_approval_step;
+DELETE FROM tbl_approval_request;
 
-TRUNCATE TABLE tbl_sales_deal_log_detail;
-TRUNCATE TABLE tbl_sales_deal_log;
-TRUNCATE TABLE tbl_deal_sked;
-TRUNCATE TABLE tbl_pers_sked;
+DELETE FROM tbl_sales_deal_log_detail;
+DELETE FROM tbl_sales_deal_log;
+DELETE FROM tbl_deal_sked;
+DELETE FROM tbl_pers_sked;
 
-TRUNCATE TABLE tbl_invoice_statement;
-TRUNCATE TABLE tbl_payment;
-TRUNCATE TABLE tbl_invoice;
-TRUNCATE TABLE tbl_statement;
+DELETE FROM tbl_invoice_statement;
+DELETE FROM tbl_payment;
+DELETE FROM tbl_invoice;
+DELETE FROM tbl_statement;
 
-TRUNCATE TABLE tbl_order_detail;
-TRUNCATE TABLE tbl_order_header;
+DELETE FROM tbl_order_detail;
+DELETE FROM tbl_order_header;
 
-TRUNCATE TABLE tbl_contract_detail;
-TRUNCATE TABLE tbl_contract_header;
+DELETE FROM tbl_contract_detail;
+DELETE FROM tbl_contract_header;
 
-TRUNCATE TABLE tbl_quotation_detail;
-TRUNCATE TABLE tbl_quotation_header;
+DELETE FROM tbl_quotation_detail;
+DELETE FROM tbl_quotation_header;
 
-TRUNCATE TABLE tbl_request_quotation_detail;
-TRUNCATE TABLE tbl_request_quotation_header;
+DELETE FROM tbl_request_quotation_detail;
+DELETE FROM tbl_request_quotation_header;
 
-TRUNCATE TABLE tbl_sales_deal;
+DELETE FROM tbl_sales_deal;
 
-TRUNCATE TABLE tbl_account_score;
-TRUNCATE TABLE tbl_client_crops;
+DELETE FROM tbl_account_score;
+DELETE FROM tbl_client_crops;
 
-TRUNCATE TABLE tbl_product_bookmark;
-TRUNCATE TABLE tbl_product_compare_item;
-TRUNCATE TABLE tbl_product_compare;
-TRUNCATE TABLE tbl_product_feedback;
-TRUNCATE TABLE tbl_product_price_history;
-TRUNCATE TABLE tbl_product_tag;
-TRUNCATE TABLE tbl_cultivation_time;
-TRUNCATE TABLE tbl_tag;
-TRUNCATE TABLE tbl_product;
+DELETE FROM tbl_product_bookmark;
+DELETE FROM tbl_product_compare_item;
+DELETE FROM tbl_product_compare;
+DELETE FROM tbl_product_feedback;
+DELETE FROM tbl_product_price_history;
+DELETE FROM tbl_product_tag;
+DELETE FROM tbl_cultivation_time;
+DELETE FROM tbl_tag;
+DELETE FROM tbl_product;
 
-TRUNCATE TABLE tbl_user;
-TRUNCATE TABLE tbl_client;
-TRUNCATE TABLE tbl_employee;
+DELETE FROM tbl_user;
+DELETE FROM tbl_client;
+DELETE FROM tbl_employee;
+
+ALTER TABLE tbl_notification_delivery AUTO_INCREMENT = 1;
+ALTER TABLE tbl_notification AUTO_INCREMENT = 1;
+ALTER TABLE tbl_approval_decision AUTO_INCREMENT = 1;
+ALTER TABLE tbl_approval_step AUTO_INCREMENT = 1;
+ALTER TABLE tbl_approval_request AUTO_INCREMENT = 1;
+ALTER TABLE tbl_sales_deal_log_detail AUTO_INCREMENT = 1;
+ALTER TABLE tbl_sales_deal_log AUTO_INCREMENT = 1;
+ALTER TABLE tbl_deal_sked AUTO_INCREMENT = 1;
+ALTER TABLE tbl_pers_sked AUTO_INCREMENT = 1;
+ALTER TABLE tbl_invoice_statement AUTO_INCREMENT = 1;
+ALTER TABLE tbl_payment AUTO_INCREMENT = 1;
+ALTER TABLE tbl_invoice AUTO_INCREMENT = 1;
+ALTER TABLE tbl_statement AUTO_INCREMENT = 1;
+ALTER TABLE tbl_order_detail AUTO_INCREMENT = 1;
+ALTER TABLE tbl_order_header AUTO_INCREMENT = 1;
+ALTER TABLE tbl_contract_detail AUTO_INCREMENT = 1;
+ALTER TABLE tbl_contract_header AUTO_INCREMENT = 1;
+ALTER TABLE tbl_quotation_detail AUTO_INCREMENT = 1;
+ALTER TABLE tbl_quotation_header AUTO_INCREMENT = 1;
+ALTER TABLE tbl_request_quotation_detail AUTO_INCREMENT = 1;
+ALTER TABLE tbl_request_quotation_header AUTO_INCREMENT = 1;
+ALTER TABLE tbl_sales_deal AUTO_INCREMENT = 1;
+ALTER TABLE tbl_account_score AUTO_INCREMENT = 1;
+ALTER TABLE tbl_client_crops AUTO_INCREMENT = 1;
+ALTER TABLE tbl_product_bookmark AUTO_INCREMENT = 1;
+ALTER TABLE tbl_product_compare_item AUTO_INCREMENT = 1;
+ALTER TABLE tbl_product_compare AUTO_INCREMENT = 1;
+ALTER TABLE tbl_product_feedback AUTO_INCREMENT = 1;
+ALTER TABLE tbl_product_price_history AUTO_INCREMENT = 1;
+ALTER TABLE tbl_product_tag AUTO_INCREMENT = 1;
+ALTER TABLE tbl_cultivation_time AUTO_INCREMENT = 1;
+ALTER TABLE tbl_tag AUTO_INCREMENT = 1;
+ALTER TABLE tbl_product AUTO_INCREMENT = 1;
+ALTER TABLE tbl_user AUTO_INCREMENT = 1;
+ALTER TABLE tbl_client AUTO_INCREMENT = 1;
+ALTER TABLE tbl_employee AUTO_INCREMENT = 1;
 
 SET FOREIGN_KEY_CHECKS = 1;
-
-START TRANSACTION;
 
 -- password1234!
 SET @bcrypt_pw = '$2y$10$nr12HMi6ppnTT6755FZtReWp/5MVHfyIp5jVE.JrwYEuLtfMaYZim';
@@ -83,7 +121,6 @@ UPDATE tbl_employee
 SET employee_code = CONCAT('EMP-', LPAD(@admin_employee_id, 4, '0'))
 WHERE employee_id = @admin_employee_id;
 
-
 INSERT INTO tbl_employee (
     employee_code,
     employee_name,
@@ -107,7 +144,6 @@ SET @sales_employee_id = LAST_INSERT_ID();
 UPDATE tbl_employee
 SET employee_code = CONCAT('EMP-', LPAD(@sales_employee_id, 4, '0'))
 WHERE employee_id = @sales_employee_id;
-
 
 INSERT INTO tbl_client (
     client_code,
@@ -152,7 +188,6 @@ SET @client_id = LAST_INSERT_ID();
 UPDATE tbl_client
 SET client_code = CONCAT('CLNT-', LPAD(@client_id, 4, '0'))
 WHERE client_id = @client_id;
-
 
 INSERT INTO tbl_user (
     login_id,
