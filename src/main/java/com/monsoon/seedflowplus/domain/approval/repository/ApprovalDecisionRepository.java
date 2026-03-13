35ef4590512aa869d5f449b122a183da2349c1cc
hatahng <hathang16@naver.com>
2026-03-13 11:54:47 +0900

    [FRT-83] feat(approval): 문서별 반려 사유 조회를 위한 Repository 쿼리 추가

--- src/main/java/com/monsoon/seedflowplus/domain/approval/repository/ApprovalDecisionRepository.java
+++ src/main/java/com/monsoon/seedflowplus/domain/approval/repository/ApprovalDecisionRepository.java
@@ -1,12 +1,22 @@
 package com.monsoon.seedflowplus.domain.approval.repository;
 
 import com.monsoon.seedflowplus.domain.approval.entity.ApprovalDecision;
+import com.monsoon.seedflowplus.domain.deal.common.DealType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ApprovalDecisionRepository extends JpaRepository<ApprovalDecision, Long> {
 
     boolean existsByApprovalStepId(Long approvalStepId);
 
     Optional<ApprovalDecision> findByApprovalStepId(Long approvalStepId);
+
+    @Query("SELECT d.reason FROM ApprovalDecision d " +
+           "JOIN d.approvalStep s " +
+           "JOIN s.approvalRequest r " +
+           "WHERE r.dealType = :dealType AND r.targetId = :targetId " +
+           "ORDER BY d.decidedAt DESC")
+    java.util.List<String> findReasonsByTarget(@Param("dealType") DealType dealType, @Param("targetId") Long targetId);
 }
